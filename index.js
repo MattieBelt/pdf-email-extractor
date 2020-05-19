bf0a6cc7fc838c8b14134b0e4507762ffcc70e9d
Mattias van den Belt <mattias@taskhero.nl>
2020-05-19 16:47:34 +0200

    Added Async working and some refactoring

--- index.js
+++ index.js
@@ -3,22 +3,15 @@
 const fs = require('fs');
 const path = require('path');
 const pdf = require('pdf-parse');
+const textract = require('textract');
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-  header: ['email'],
-  path: 'results.csv'
-});
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
 
-const betterQueue = require('better-queue');
+let fileCount = 0, filesProcessed = 0, nonPDFs = [], errors = [];
 
-let writeQueue = new betterQueue( (input, cb) => {
-  console.log(input);
-  saveEmail(input);
-
-  cb(null, true);
-})
-
+const queue = [];
+const outputPath = 'results.csv';
+const rootPath = '/home/taskhero/Downloads/pdf-drive';
 
 /**
  * @param {string} parentPath
@@ -26,73 +19,110 @@ let writeQueue = new betterQueue( (input, cb) => {
  */
 // TODO: add starting point
 function walk(parentPath, execute) {
+  if (fileCount > 1000) return;
 
-  const dir = fs.readdirSync(parentPath);
+  fs.readdir(parentPath, (error, dir) => {
+      if (error) {
+        errors.push({parentPath, error});
+        return false;
+      }
 
-  let childPath = '';
-  for (let i = 0; i < dir.length; i++) {
-    childPath = path.resolve(parentPath, dir[i]);
+      fileCount = fileCount + dir.length;
+      logProcess();
 
-    if (fs.statSync(childPath).isDirectory()) {
-      walk(childPath, execute);
-    } else {
-      console.log('non-dir: ' + childPath);
-      execute(childPath);
+
+      dir.forEach((child) => {
+
+        const childPath = path.resolve(parentPath, child);
+
+        fs.stat(childPath, (error, stats) => {
+          if (error) {
+            errors.push({childPath, error});
+            return false;
+          }
+
+
+          if (stats.isDirectory()) {
+            filesProcessed++;
+            walk(childPath, execute);
+          } else {
+            execute(childPath);
+          }
+        });
+      });
     }
-  }
+  );
 }
 
 /**
  * @param {string} filePath
  */
-function read(filePath) {
-  if (path.extname(filePath)) {
-    const dataBuffer = fs.readFileSync(filePath);
+function extractEmail(filePath) {
+  if (path.extname(filePath) === '.pdf') {
+    fs.readFile(filePath, (error, buffer) => {
+      if (error) {
+        errors.push({filePath, error});
+        return false;
+      }
 
-    pdf(dataBuffer).then((data) => {
-      const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
+      pdf(buffer)
+        .then((data) => {
+          let emailsString = '';
+          const emails = data.text.match(emailRegex);
 
-      data.text.match(regex).forEach((email) => {
-        console.log(email);
-
-        writeQueue.push(email)
-      });
+          if (emails !== null) {
+            emails.forEach((email) => {
+              emailsString += email + '\r\n';
+            });
+            queue.push(
+              () => {
+                try {
+                  fs.appendFileSync(outputPath, emailsString);
+                  filesProcessed++;
+                } catch (appendError) {
+                  console.log('Error while trying to save emails to csv: ' + appendError);
+                }
+              });
+          } else {
+            filesProcessed++;
+          }
+        })
+        .catch((pdfError) => {
+          errors.push({filePath, pdfError});
+        });
     });
+
   } else {
-    // TODO: handle non-pdfs
+    nonPDFs.push(filePath);
+    filesProcessed++;
   }
 }
 
-function saveEmail(email) {
-  csvWriter.writeRecords([{email}])
-    .then(() => {
-      console.log('...saved');
-    });
+function handleQueue() {
+
+  while (queue.length) {
+    //Get first function
+    const first = queue.shift();
+
+    //Execute first function
+    first();
+
+    logProcess();
+  }
+
 }
 
+function logProcess() {
+  console.log(`Files processed: (${filesProcessed}/${fileCount})`);
+  console.log(nonPDFs);
+  console.log(errors);
+}
 
-const rootPath = './sources';
+walk(rootPath, extractEmail);
 
-walk(rootPath, read);
+setTimeout(handleQueue, 10000)
 
-
-// Loop through files
-// * subfolder
 // * Starting point
-
-
-
-
-
-// Check if PDF
-// * note NON-PDF's
-
-
-// Parse PDF -> string
-
-
-// Check string for emails
-// * REGEX
 
 // Save emails
 // * print to csv
